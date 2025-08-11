8c28d4e236662582b91c353b38c32e763f2f4935
Sandeep Sagar <sandeep.sagar033@gmail.com>
2025-08-11 17:09:33 -0500

    initial setup

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,43 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { Header } from "./header/header";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [Header],
 })
 export class App {
-  protected readonly title = signal('angular-deep-dive');
+  dummyTrafficData = [
+    {
+      id: 'd1',
+      value: 433,
+    },
+    {
+      id: 'd2',
+      value: 260,
+    },
+    {
+      id: 'd3',
+      value: 290,
+    },
+    {
+      id: 'd4',
+      value: 410,
+    },
+    {
+      id: 'd5',
+      value: 397,
+    },
+    {
+      id: 'd6',
+      value: 488,
+    },
+    {
+      id: 'd47',
+      value: 589,
+    },
+  ];
+  maxTraffic = Math.max(...this.dummyTrafficData.map((data) => data.value));
+  currentStatus = 'online';
 }
---
+++ src/app/dashboard/traffic/traffic.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-traffic-status',
+  imports: [],
+  templateUrl: './traffic.html',
+  styleUrl: './traffic.css'
+})
+export class TrafficStatus {
+
+}
